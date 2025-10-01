4284306a52e29a96062a44cd5cec7576741e0eab
Nikhil singh Rathore <nikhils1620@gmail.com>
2025-10-02 00:43:10 +0530

    add

--- chitchat/accounts/consumers.py
+++ chitchat/accounts/consumers.py
@@ -17,6 +17,11 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name,
             self.channel_name
         )
+        
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
 
 
@@ -25,6 +30,10 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name,
             self.channel_name
         )
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
+
 
      
          
@@ -54,3 +63,16 @@ class ChatroomConsumer(WebsocketConsumer):
         html = render_to_string("accounts/partial/chatnew.html", context=context)
         self.send(text_data=html)
 
+    def update_online_count(self):
+        online_count=self.chatroom.users_online.count()
+        
+        event={
+            'type':'online_count_handler',
+            'online_count':online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name,event)
+
+    def online_count_handler(self,event):
+        online_count=event['online_count']    
+        html = render_to_string("accounts/partial/online_count.html", {'online_count':online_count})
+        self.send(text_data=html)
